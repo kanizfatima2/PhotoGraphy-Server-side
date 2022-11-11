6db3aa136d374dc02cc6b81a0a3e79e5c46c66dc
kanizfatima2 <kaniz686928@gmail.com>
2022-11-11 23:09:27 +0600

    s

--- index.js
+++ index.js
@@ -20,6 +20,159 @@ if (uri) {
     console.log('Database Connected')
 }
 
+//Run function
+async function run() {
+    const Service = client.db('PhotographyService').collection('services')
+    const Reviews = client.db('PhotographyService').collection('reviews')
+    const newService = client.db('PhotographyService').collection('newService')
+    try {
+
+        // get all Services 
+        app.get('/services', async (req, res) => {
+            try {
+                const cursor = Service.find({})
+                const services = await cursor.toArray();
+
+                res.send({
+                    success: true,
+                    message: 'Successfully read the data!',
+                    data: services
+                })
+            }
+            catch (error) {
+                console.log(error.message);
+                res.send({
+                    success: false,
+                    error: error.message
+                })
+            }
+        })
+
+        //Read One Service
+        app.get('/services/:id', async (req, res) => {
+            const { id } = req.params;
+            try {
+                const service = await Service.findOne({ _id: ObjectId(id) })
+
+                res.send({
+                    success: true,
+                    message: 'Successfully read the data!',
+                    data: service
+                })
+            }
+            catch (error) {
+                console.log(error.message);
+                res.send({
+                    success: false,
+                    error: error.message
+                })
+            }
+        })
+
+        //Create Reviews
+        app.post('/reviews', async (req, res) => {
+            const result = await Reviews.insertOne(req.body)
+            if (result.insertedId) {
+                res.send({
+                    success: true,
+                    message: 'Succesfully placed the order!!'
+                })
+            }
+            else {
+                res.send({
+                    success: false,
+                    error: 'could not place the order!'
+                })
+            }
+        })
+        //Create Services
+        app.post('/newService', async (req, res) => {
+            const result = await newService.insertOne(req.body)
+            if (result.insertedId) {
+                res.send({
+                    success: true,
+                    message: 'Succesfully placed the order!!'
+                })
+            }
+            else {
+                res.send({
+                    success: false,
+                    error: 'could not place the order!'
+                })
+            }
+        })
+
+        //Read all reviews
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+
+
+            try {
+                if (req.query.email) {
+                    query = {
+                        email: req.query.email
+                    }
+                }
+                const cursor = Reviews.find(query);
+                const reviews = await cursor.toArray();
+                res.send({
+                    success: true,
+                    message: 'Successfully read all reviews',
+                    data: reviews
+                })
+            }
+            catch (error) {
+                console.log(error.message);
+                res.send({
+                    success: false,
+                    error: error.message
+                })
+            }
+
+        })
+
+        //Delete Reviews
+        app.delete('/reviews/:id', async (req, res) => {
+            const { id } = req.params;
+            try {
+                const reviews = await Reviews.findOne({ _id: ObjectId(id) })
+
+                //Review does not exist
+                if (!reviews?._id) {
+                    res.send({
+                        success: false,
+                        error: "No Review",
+                    });
+                    return;
+                }
+
+                //Review Exist
+                const result = await Reviews.deleteOne({ _id: ObjectId(id) });
+
+                if (result.deletedCount) {
+                    res.send({
+                        success: true,
+                        message: `Successfully deleted the ${reviews.title}`,
+                    });
+                }
+            }
+
+            catch (error) {
+                res.send({
+                    success: false,
+                    error: error.message,
+                });
+            }
+        })
+
+
+    }
+
+    finally {
+
+    }
+}
+run().catch(err => console.error(err.message))
 
 
 app.get('/', (req, res) => {
